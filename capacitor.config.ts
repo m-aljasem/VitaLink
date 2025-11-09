27f32c5c9fe87e2efcf4b6897460751fbf205cb8
Mohamad <m-aljasem@outlook.com>
2025-11-09 18:01:24 +0330

    getting ready for android builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.aljasem.vitalink',
   appName: 'VitaLink',
-  webDir: 'www'
+  webDir: 'www',
+  // Custom icons are replaced during build process in codemagic.yaml
 };
 
 export default config;
