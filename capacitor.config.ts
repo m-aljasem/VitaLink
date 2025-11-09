a965c8936f49c9f72116dcf013798307303a1cbb
Mohamad <m-aljasem@outlook.com>
2025-11-09 16:43:06 +0330

    getting ready for android builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'org.aljasem.vitalink',
   appName: 'VitaLink',
   webDir: 'www'
 };
